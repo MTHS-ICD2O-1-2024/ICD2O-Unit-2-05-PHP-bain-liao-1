14f9bd8043f71cd02058ba5c2e6634ffe7cf76d8
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-03-25 16:59:00 +0000

    created salary program in php

---
+++ answer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit1-08-HTML-MDL -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Salary, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Bain Liao" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.light_blue-orange.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Salary, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Salary, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/income_tax.jpg" alt="income" />
+      </div>
+      <div class="page-content">
+        <div id="user-info">
+          <?php
+          $TAX_RATE = 0.18;
+          $hoursWorked = $_GET["hours"];
+          $hourlyWage = $_GET["rate"];
+
+          // process
+          $taxes = ($hoursWorked * $hourlyWage) * $TAX_RATE;
+          $takeHomeSalary = $hourlyWage * $hoursWorked - $taxes;
+
+          // output
+          echo "Your pay will be: $" . (round($takeHomeSalary, 2));
+          echo "<br />";
+          echo "The government will take: $" . (round($taxes, 2));
+          ?>
+        </div>
+        <br />
+        <div class="page-content-answer">
+          <a href="./index.php">Return ...</a>
+        </div>
+    </main>
+  </div>
+</body>
+
+</html>
